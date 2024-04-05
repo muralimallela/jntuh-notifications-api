f100523b6ed5606b8d4e8e89bda0c2aeb4efd1c8
Murali Krishna <mmallela9@gmail.com>
2024-04-05 21:16:52 +0530

    initail commit

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, Query,status
 from bs4 import BeautifulSoup
 import requests
 import re
+import uvicorn
 from os import getenv
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/", methods=["GET", "HEAD"])
+@app.get("/",status_code=status.HTTP_200_OK)
 def root():
-    return jsonify({"message": "Visit URL/docs for API documentation"}), 200
-
-@app.route("/notifications", methods=["GET"])
-def fetch_data():
-    query = request.args.get("query")
+    return {"message" : "visit URL/docs for api documentation"}
+@app.get("/notifications",status_code=status.HTTP_200_OK)
+async def fetch_data(query: str = Query(None)):
     url = "http://61.1.174.28/jsp/RCRVInfo.jsp"
-    
     try:
         response = requests.get(url)
-        
         if response.status_code == 200:
             html_content = response.text
             soup = BeautifulSoup(html_content, 'html.parser')
             h3_tags = soup.find_all('h3')
             content = []
-            
             for h3_tag in h3_tags:
                 text = h3_tag.text.strip()
                 content.append(text)
-            
             string_list = content
             pattern = r'\*?\((.*?)\)\s+(.*?)\.+\s+LAST DATE TO APPLY FOR\s+(.*?):\s+(\d{2}-\d{2}-\d{4})'
             result_list = []
-            
             for string in string_list:
                 match = re.match(pattern, string)
-                
                 if match:
                     release_date = match.group(1)
                     notification = match.group(2)
@@ -45,17 +38,17 @@ def fetch_data():
                         "warning": f"Last date to apply for {match.group(3)} is {end_date}"
                     }
                     result_list.append(result)
-            
             if query:
                 filtered_results = [result for result in result_list if query.lower() in result['release_date'].lower() or query.lower() in result['notification'].lower() or query.lower() in result['warning'].lower()]
-                return jsonify(filtered_results), 200
+                return filtered_results
             else:
-                return jsonify(result_list), 200
+                return result_list
         else:
-            return jsonify({"message": f"Failed to fetch the HTML content. Status code: {response.status_code}"}), 500
+            return {"message": f"Failed to fetch the HTML content. Status code: {response.status_code}"}
     except requests.RequestException as e:
-        return jsonify({"message": f"Error fetching the HTML content: {e}"}), 500
+        return {"message": f"Error fetching the HTML content: {e}"}
+
 
 if __name__ == "__main__":
-    port = int(getenv("PORT", 8000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(getenv("PORT",8000))
+    uvicorn.run("main:app",host="0.0.0.0",port=port,reload=True)
