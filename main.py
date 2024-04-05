2cd416e4e45642e0376f91316a9de6435c63db01
Murali Krishna <mmallela9@gmail.com>
2024-04-05 22:05:21 +0530

    initail commit

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ import requests
 import re
 import uvicorn
 from os import getenv
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/",status_code=status.HTTP_200_OK)
 def root():
